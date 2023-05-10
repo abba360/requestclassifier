69471e4ac0ccf05394cd5774940cea5007ac29d4
ISHAQ HASSAN <81328045+abba360@users.noreply.github.com>
2023-05-10 09:59:01 +0100

    Update settings.py

--- requestclassifier/settings.py
+++ requestclassifier/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2txg)_cr$vx^v=7%6@+4u85nvd2-^j1jiv$&y(v33w%6k%n7y('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
